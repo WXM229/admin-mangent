23f654258d3b9aa510412fb1764bf8f952094a82
王晓敏 <wangxiaomin@xiaoyezi.com>
2021-05-24 16:25:41 +0800

    feature checkout

--- src/views/myPage/index.vue
+++ src/views/myPage/index.vue
@@ -2,16 +2,23 @@
   <div>
     <el-card>
       <slot name="header"></slot>
+      <el-checkbox-group v-model="checkList">
+         <el-checkbox v-for="item in checkData" :label="item" :key="item">{{item}}</el-checkbox>
+      </el-checkbox-group>
     </el-card>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      checkList: [],
+      checkData: ["选中且禁用", "复选框 A"],
+    };
+  },
+};
 </script>
 
 <style>
-
 </style>
