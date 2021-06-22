a89394e9d296951336e5ba542fd413efdc94daeb
王晓敏 <wangxiaomin@xiaoyezi.com>
2021-06-22 15:44:49 +0800

    fix router

--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,6 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
-    // redirect: '/home/index'
     redirect: '/login'
   },
   {
--- src/views/home/classList.vue
+++ src/views/home/classList.vue
@@ -1,21 +1,14 @@
 <template>
   <div class="page">
-    <Index>
-      <h1 slot="header">这是父组件的slot替代内容</h1>
-    </Index>
   </div>
 </template>
 
 <script>
-import Index from '../myPage/index'
 export default {
   data() {
     return {
 
     };
-  },
-  components: {
-    Index
   }
 };
 </script>
