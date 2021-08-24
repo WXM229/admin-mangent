752118ccfb8231971bf20ae9ab57f574e833c7b8
王晓敏 <wangxiaomin@xiaoyezi.com>
2021-08-24 18:45:05 +0800

    add slot content

---
+++ src/components/tools/searchField.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <el-card class="card">
+      <slot name="header"></slot>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped xml:lang="less">
+.card {
+  margin-bottom: 10px;
+}
+</style>
