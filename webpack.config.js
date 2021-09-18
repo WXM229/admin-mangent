5704f68935a23c99485059f6e76b37e4be66d02c
王晓敏 <wangxiaomin@xiaoyezi.com>
2021-09-18 15:16:15 +0800

    npm install sass

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,10 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.sass$/,
+        use: ['vue-style-loader', 'css-loader', 'sass-loader']
+      },
     ],
   },
 };
